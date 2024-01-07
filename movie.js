bb10ff73d762509abfc67e0e1ee5d4d9da87e0ac
seoyeon-1206 <ssy_1206@naver.com>
2024-01-07 14:13:13 +0900

    [MovieApp] 영화 검색 기능 구현

--- movie.js
+++ movie.js
@@ -42,3 +42,42 @@ const options = {
     })
     .catch(err => console.error(err));
 
+
+    const handleSearch = () => {
+        fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+        .then(response => response.json())
+        .then(response => {
+            const searchValue = document.getElementById("search-input").value 
+            const filteredMovies = response.results.filter(item => item.title.includes(searchValue));
+    
+            const cardListElem = document.getElementById("card-list")
+            cardListElem.innerHTML = ''
+            filteredMovies.forEach(element => {
+                let newItem = document.createElement("div")
+                let titleElem = document.createElement("h3")
+                let overviewElem = document.createElement("p")
+                let voteElem = document.createElement("p")
+                let posterElem = document.createElement("img")
+                newItem.classList.add("movie-card")
+                posterElem.classList.add("poster")
+                titleElem.classList.add("title")
+                voteElem.classList.add("vote")
+                titleElem.innerText = element.title
+                overviewElem.innerText = element.overview
+                voteElem.innerText = element.vote_average
+                posterElem.src = `${baseURL}${element.poster_path}`
+                newItem.appendChild(posterElem)
+                newItem.appendChild(titleElem)
+                newItem.appendChild(overviewElem)
+                newItem.appendChild(voteElem)
+                newItem.setAttribute('id', element.id);
+                document.getElementById("card-list").appendChild(newItem)
+                newItem.addEventListener('click', () => {
+                    const movieId = newItem.getAttribute('id')
+                    alert(`이 영화의 ID는 ${movieId}입니다`);
+                });
+            });
+        })
+        .catch(err => console.error(err));
+    }
+    
